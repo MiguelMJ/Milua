77aa645210e8aa4e001cc5187a2060918b827c9e
Danilo Hoffmann <dhhyi@aol.com>
2023-07-19 07:30:51 +0000

    various improvements

--- examples/handsome_server.lua
+++ examples/handsome_server.lua
@@ -1,11 +1,13 @@
-local app = require("../src")
+local app = require("milua")
 
 -- Basic example
 app.add_handler(
     "GET",
     "/",
     function()
-        return "<h1>Welcome to the handsome server!</h1>"
+        return "<h1>Welcome to the <i>handsome</i> server!</h1>", {
+            ["Content-Type"] = "text/html"
+        }
     end
 )
 
@@ -14,11 +16,18 @@ app.add_handler(
     "GET",
     "/user/...", 
     function (captures, query, headers)
-
         local username = captures[1]
         local times = query.times or 1
         return "The user " .. username .. " is" .. (" very"):rep(times) .. " handsome"
-    
+    end
+)
+
+-- Example returning no data and status
+app.add_handler(
+    "DELETE",
+    "/user",
+    function ()
+        return nil, { [":status"] = "204" }
     end
 )
 
